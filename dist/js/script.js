4ebc38700162d0707a8decc15b0aad02671d7180
danger-enemy <ozerov.webmater496@gmail.com>
2020-09-26 21:25:39 +0300

    1-st anim is ready

--- dist/js/script.js
+++ dist/js/script.js
@@ -29,6 +29,9 @@ window.addEventListener('load', trimmer)
 const creativeRhombus = document.querySelector('.header__rhombus-blue');
 const creativeRhombusMini = document.querySelector('.header__rhombus-black');
 const headerTitle = document.querySelector('.header__title');
+const bestBlock = document.querySelector('.rmb-best');
+const bigBlock = document.querySelector('.rmb-bg');
+const miniBlock = document.querySelector('.rmb-mini');
 
 
 
@@ -37,6 +40,9 @@ window.addEventListener('load', function(){
     creativeRhombus.classList.add('_blue-onload');
     creativeRhombusMini.classList.add('_black-transition', '_black-onload');
     headerTitle.classList.add('_title-onload');
+    bestBlock.classList.add('_rmb-load');
+    bigBlock.classList.add('_rmb-bg-onload');
+    miniBlock.classList.add('_rmb-mini-onload');
 });
 
 
--- source/js/animation.js
+++ source/js/animation.js
@@ -1,6 +1,9 @@
 const creativeRhombus = document.querySelector('.header__rhombus-blue');
 const creativeRhombusMini = document.querySelector('.header__rhombus-black');
 const headerTitle = document.querySelector('.header__title');
+const bestBlock = document.querySelector('.rmb-best');
+const bigBlock = document.querySelector('.rmb-bg');
+const miniBlock = document.querySelector('.rmb-mini');
 
 
 
@@ -9,4 +12,7 @@ window.addEventListener('load', function(){
     creativeRhombus.classList.add('_blue-onload');
     creativeRhombusMini.classList.add('_black-transition', '_black-onload');
     headerTitle.classList.add('_title-onload');
+    bestBlock.classList.add('_rmb-load');
+    bigBlock.classList.add('_rmb-bg-onload');
+    miniBlock.classList.add('_rmb-mini-onload');
 })
